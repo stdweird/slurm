19dda49cbbd4de21fdc0b457a028d1547d8f14e8
stdweird <stijn.deweirdt@ugent.be>
2019-08-15 14:32:44 +0200

    build with pmix, ucx and nvml

--- bdist_rpm.sh
+++ bdist_rpm.sh
@@ -14,5 +14,7 @@ GITTAG=$(git log --format=%ct.%h -1)
 mkdir -p BUILD SOURCES SPECS RPMS BUILDROOT
 git archive --format=tar.gz -o "SOURCES/slurm-${SUFFIX}.tar.gz" --prefix="slurm-${SUFFIX}/" HEAD
 cp slurm.spec "SPECS"
-rpmbuild --define "gittag ${GITTAG}" --define "_topdir $PWD" -ba SPECS/slurm.spec
 
+sudo yum install -y nvidia-driver-NVML ucx-devel pmix-devel numactl-devel hwloc-devel
+# there's no option to pass nvml, it is only autodetected
+rpmbuild --define "gittag ${GITTAG}" --define "_topdir $PWD" --with numa --with pmix --with hwloc --with x11 --with ucx -ba SPECS/slurm.spec
