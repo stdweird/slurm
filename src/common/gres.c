ec6382a2f68a61044a2ee84ed66f6d42c27bd748
stdweird <stijn.deweirdt@ugent.be>
2019-09-25 13:02:37 +0200

    Revert "apply patch from issue 7726 (gpu-bind=closest)"
    This reverts commit 8bbc5144e9b1d62a77934289f33adc385cbf2212.

--- src/common/gres.c
+++ src/common/gres.c
@@ -123,8 +123,7 @@ typedef struct slurm_gres_ops {
 						  int local_proc_id );
 	void		(*step_reset_env)	( char ***job_env_ptr,
 						  void *gres_ptr,
-						  bitstr_t *usable_gres,
-					          int local_proc_id );
+						  bitstr_t *usable_gres );
 	void		(*send_stepd)		( int fd );
 	void		(*recv_stepd)		( int fd );
 	int		(*job_info)		( gres_job_state_t *job_gres_data,
@@ -12185,7 +12184,7 @@ extern void gres_plugin_step_set_env(char ***job_env_ptr, List step_gres_list,
 					(*(gres_context[i].ops.step_reset_env))
 						(job_env_ptr,
 						 gres_ptr->gres_data,
-						 usable_gres, local_proc_id);
+						 usable_gres);
 				} else {
 					(*(gres_context[i].ops.step_set_env))
 						(job_env_ptr,
@@ -12200,7 +12199,7 @@ extern void gres_plugin_step_set_env(char ***job_env_ptr, List step_gres_list,
 		if (!found) { /* No data fond */
 			if (accel_bind_type || tres_bind) {
 				(*(gres_context[i].ops.step_reset_env))
-					(job_env_ptr, NULL, NULL, 0); /* Fixme*/
+					(job_env_ptr, NULL, NULL); /* Fixme */
 			} else {
 				(*(gres_context[i].ops.step_set_env))
 					(job_env_ptr,
