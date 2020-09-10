2de8c17d747d0e1bf426b1cfd8cdbdb20da1c33d
stdweird <stdweird@regi8.metapod.os>
2020-09-10 20:50:33 +0200

    add nogpu + el8

--- bdist_rpm-no-gpu.sh
+++ bdist_rpm-no-gpu.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 set -e
+set -x
 
-VERSION=`grep "Version:.*[0-9]" slurm.spec | tr -s " " |  awk '{print $2;}'`
-RELEASE=`grep "%global rel.*[-1-9]" slurm.spec | tr -s " " | awk '{print $3}'`
+SPEC=slurm-no-gpu.spec
+
+VERSION=`grep "Version:.*[0-9]" $SPEC | tr -s " " |  awk '{print $2;}'`
+RELEASE=`grep "%global rel.*[-1-9]" $SPEC | tr -s " " | awk '{print $3}'`
 
 if [ ${RELEASE} != "1" ]; then
     SUFFIX=${VERSION}-${RELEASE}
@@ -15,7 +18,7 @@ GITTAG=$(git log --format=%ct.%h -1)
 
 mkdir -p BUILD SOURCES SPECS RPMS BUILDROOT
 git archive --format=tar.gz -o "SOURCES/slurm-${SUFFIX}.tar.gz" --prefix="slurm-${SUFFIX}/" HEAD
-cp slurm.spec "SPECS"
+cp $SPEC "SPECS"
 
 # there's no option to pass nvml, it is only autodetected
 # nvidia-driver-devel provides the libnividia-ml.so symlnk, the real .so.1 comes from nvidia-driver-NVML
@@ -25,7 +28,12 @@ cp slurm.spec "SPECS"
 
 # TODO: what if more than one cuda is available/installed, then the * thingies will probably not work
 # pmix-3 as rebuild from github src.rpm includes the devel rpms in the rpm
-sudo yum install -y nvidia-driver-devel cuda-nvml-dev-10-2 nvidia-driver-NVML ucx-devel pmix-devel-3.* numactl-devel hwloc-devel
+sudo yum remove nvidia-driver-devel cuda-nvml-dev-10-2 nvidia-driver-NVML 
+sudo yum install ucx-devel "pmix-devel > 3.0.0" numactl-devel hwloc-devel
+
+# remove json-c12 -> use plain json-c
+sudo yum install json-c-devel
+# also, specify python2 as python to require
 
 # glob expansion in list
 nvmls=(/usr/local/cuda*/targets/x86_64-linux/include)
@@ -35,4 +43,4 @@ if [ "${#nvmls[@]}" -ne 1 ]; then
 fi
 
 #rpmbuild --define "gittag ${GITTAG}" --define "_topdir $PWD" --with numa --with pmix --with hwloc --with x11 --with ucx -ba SPECS/slurm.spec --define "_configure ./configure CPATH=${nvmls[0]}" --define "_smp_mflags CPATH=${nvmls[0]}"
-rpmbuild --define "gittag ${GITTAG}" --define "_topdir $PWD" --with numa --with pmix --with hwloc --with mysql --with x11 --with ucx -ba SPECS/slurm.spec --define "_configure ./configure CPATH=${nvmls[0]}" --define "_smp_mflags CPATH=${nvmls[0]}"
+rpmbuild --define "gittag ${GITTAG}" --define "_topdir $PWD" --with pmix --with numa --with hwloc --with mysql --with x11 --with ucx -ba SPECS/$SPEC --define "_configure ./configure CPATH=${nvmls[0]}" --define "_smp_mflags CPATH=${nvmls[0]}"
