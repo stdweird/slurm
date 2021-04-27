ef6e9d3b73eca5a965fb26daf6ea44fc8f48c0b7
Andy Georges <itkovian@gmail.com>
2021-04-27 12:15:21 +0200

    cleanup: remove unneeded nogpu build args

--- bdist_rpm.sh
+++ bdist_rpm.sh
@@ -69,19 +69,13 @@ cp $SPEC "SPECS"
 
 # TODO: what if more than one cuda is available/installed, then the * thingies will probably not work
 # pmix-3 as rebuild from github src.rpm includes the devel rpms in the rpm
-sudo yum remove -y nvidia-driver-devel cuda-nvml-dev-10-2 nvidia-driver-NVML 
+sudo yum remove -y nvidia-driver-devel cuda-nvml-dev-10-2 nvidia-driver-NVML cuda-nvml-dev-10-1
 sudo yum install -y ucx-devel "pmix-devel > 3.0.0" numactl-devel hwloc-devel
 
 # remove json-c12 -> use plain json-c
 sudo yum install -y json-c-devel json-c
 # also, specify python2 as python to require
 
-# glob expansion in list
-nvmls=(/usr/local/cuda*/targets/x86_64-linux/include)
-if [ "${#nvmls[@]}" -ne 1 ]; then
-    echo "0 or more than one nvml.h found: ${nvmls[@]}. Unsupported."
-    exit 1
-fi
 
 #rpmbuild --define "gittag ${GITTAG}" --define "_topdir $PWD" --with numa --with pmix --with hwloc --with x11 --with ucx -ba SPECS/slurm.spec --define "_configure ./configure CPATH=${nvmls[0]}" --define "_smp_mflags CPATH=${nvmls[0]}"
-rpmbuild --define "gittag ${GITTAG}" --define "_topdir $PWD" --with pmix --with numa --with hwloc --with mysql --with x11 --with ucx -ba SPECS/$SPEC --define "_configure ./configure CPATH=${nvmls[0]}" --define "_smp_mflags CPATH=${nvmls[0]}"
+rpmbuild --define "gittag ${GITTAG}" --define "_topdir $PWD" --with pmix --with numa --with hwloc --with mysql --with x11 --with ucx -ba SPECS/$SPEC
